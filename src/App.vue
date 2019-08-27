1142b943fd3b9340c7ff3d0b25874661422be7c2
EasonApolo <787817128@qq.com>
2019-08-27 11:28:56 +0800

    0.0.1
    init project
    draw board

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <canvas id='canvas' style='width:100%;height:100%' :width='w' :height='h'></canvas>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      canvas: undefined,
+      ctx: undefined,
+      w: 0,
+      h: 0,
+      board: {
+        grid: [[], [], [], [], [], []]
+      }
+    }
+  },
+  created () {
+    this.w = document.documentElement.clientWidth*2
+    this.h = document.documentElement.clientHeight*2
+    this.initBoard()
+  },
+  mounted () {
+    this.canvas = document.getElementById('canvas')
+    this.ctx = this.canvas.getContext('2d')
+    this.drawBoard()
+  },
+  methods: {
+    initBoard () {
+      for (let i in this.board.grid) {
+        for (let j = 0; j < 7; j++) {
+          this.board.grid[i].push(undefined)
+        }
+      }
+    },
+    drawBoard () {
+      let ctx = this.ctx
+      const bMarTop = 300
+      const bw = 1600
+      const bh = 1200
+      const gr = 120
+      const ratio = Math.sqrt(3) / 2
+      this.ctx.strokeStyle = 'rgb(150, 150, 150)'
+      this.ctx.strokeRect(this.w/2-bw/2, bMarTop, bw, bh)
+      for (let i in this.board.grid) {
+        let bias = i % 2 == 1 ? bias = ratio*gr/2 : -ratio*gr/2
+        let cenT = bh/2+bMarTop+(i-2.5)*1.5*gr
+        for (let j in this.board.grid[i]) {
+          let cenL = this.w/2+(j-3)*ratio*2*gr+bias
+          ctx.beginPath()
+          for (let k =0; k < 6; k++) {
+            let rad = Math.PI*(1/3*k-1/6)
+            let x = Math.cos(rad) * gr
+            let y = Math.sin(rad) * gr
+            this.ctx.lineTo(cenL+x, cenT+y)
+          }
+          ctx.closePath()
+          ctx.strokeStyle = 'rgb(100, 100, 100)'
+          ctx.stroke()
+        }
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss">
+html {
+  height: 100%;
+}
+body {
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
