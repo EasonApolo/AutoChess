22690f60b956dff6c3c8bba1f098cbd42a4dc2e9
EasonApolo <787817128@qq.com>
2019-08-27 21:08:31 +0800

    0.0.3
    add hand_frame
    add click_func
    add store_button
    add test_data

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
-    <canvas id='canvas' style='width:100%;height:100%' :width='w' :height='h'></canvas>
+    <canvas id='canvas' style='width:100%;height:100%' :width='w' :height='h' @click='click'></canvas>
   </div>
 </template>
 
 <script>
+import CardInfo from './assets/card.json'
+import TypeInfo from './assets/type.json'
+import PosInfo from './assets/position.js'
+
 export default {
   name: 'app',
   data () {
@@ -13,15 +17,29 @@ export default {
       ctx: undefined,
       w: 0,
       h: 0,
+      game: {
+        turn: 0,
+        gold: 1,
+        lvl: 1,
+        costUpgrade: 4,
+        costRedeal: 2
+      },
       board: {
         grid: [[], [], [], [], [], []]
       },
       store: {
         cards: [{}, {}, {}, {}, {}]
       },
+      hand: {
+        cards: [{}, {}, {}, {}, {}, {}, {}, {}, {}]
+      },
       color: {
         section: 'rgb(250, 250, 250)',
-        obj: 'rgb(100, 100, 100)'
+        obj: 'rgb(100, 100, 100)',
+        cardText: 'rgb(66, 66, 66)',
+        reDealButton: 'rgb(199, 99, 99)',
+        upgradeButton: 'rgb(99, 99, 199)',
+        gold: '#ffd700'
       }
     }
   },
@@ -33,7 +51,9 @@ export default {
   mounted () {
     this.canvas = document.getElementById('canvas')
     this.ctx = this.canvas.getContext('2d')
+    this.deal()
     this.drawBoard()
+    this.drawHand()
     this.drawStore()
   },
   methods: {
@@ -44,28 +64,36 @@ export default {
         }
       }
     },
-    drawStore () {
-      let ctx = this.ctx
-      const bw = 1800
-      const bh = 300
-      const cw = 260
-      let strL = this.w/2-bw/2
-      let strT = this.h-bh
-      ctx.fillStyle = this.color.section
-      ctx.fillRect(strL, strT, bw, bh)
-      let cards = this.store.cards
-      for (let i in cards) {
-        ctx.strokeStyle = this.color.obj
-        ctx.strokeRect(strL + bw + (i - 5) * cw + (i - 5) * 20, strT + 20, cw, cw)
+    click (e) {
+      let x = e.clientX*2
+      let y = e.clientY*2
+      let info = PosInfo.board
+      if (x>this.w/2-info.w/2 && x<this.w/2+info.w/2 && y>info.marTop && y<info.marTop+info.h) {
+        console.log('board')
+      }
+      info = PosInfo.hand
+      if (x>this.w/2-info.w/2 && x<this.w/2+info.w/2 && y>info.marTop && y<info.marTop+info.h) {
+        console.log('hand')
+      }
+      info = PosInfo.store
+      if (x>this.w/2-info.w/2 && x<this.w/2+info.w/2 && y>this.h-info.h && y<this.h) {
+        console.log('store')
+      }
+    },
+    deal () {
+      for (let i = 0; i < 5; i++) {
+        let cardId = Math.floor(Math.random()*CardInfo.length)
+        this.store.cards[i] = CardInfo[cardId]
       }
     },
     drawBoard () {
       let ctx = this.ctx
-      const bMarTop = 200
-      const bw = 1500
-      const bh = 1100
-      const gr = 110
-      const ratio = Math.sqrt(3) / 2
+      let info = PosInfo.board
+      const bMarTop = info.marTop
+      const bw = info.w
+      const bh = info.h
+      const gr = info.w1
+      const ratio = info.ratio
       ctx.fillStyle = this.color.section
       ctx.fillRect(this.w/2-bw/2, bMarTop, bw, bh)
       for (let i in this.board.grid) {
@@ -85,7 +113,68 @@ export default {
           ctx.stroke()
         }
       }
-    }
+    },
+    drawHand () {
+      let ctx = this.ctx
+      let info = PosInfo.hand
+      const cw = info.w1
+      const sp = info.sp
+      const hw = info.w
+      const hh = info.h
+      const handT = info.marTop
+      let handL = this.w/2-hw/2
+      let cards = this.hand.cards
+      ctx.fillStyle = this.color.section
+      ctx.fillRect(handL, handT, hw, hh)
+      for (let i in cards) {
+        ctx.strokeStyle = this.color.obj
+        let cardL = this.w / 2 + (i-4-0.5)*cw+(i-4)*sp
+        let cardT = handT + sp
+        ctx.strokeRect(cardL, cardT, cw, cw)
+      }
+    },
+    drawStore () {
+      let ctx = this.ctx
+      let info = PosInfo.store
+      const bw = info.w
+      const bh = info.h
+      const cw = info.w1
+      const sp = info.sp
+      const bth = info.bh
+      let strL = this.w/2-bw/2
+      let strT = this.h-bh
+      ctx.fillStyle = this.color.section
+      ctx.fillRect(strL, strT, bw, bh)
+      let cards = this.store.cards
+      for (let i in cards) {
+        ctx.strokeStyle = this.color.obj
+        let cardL = strL + bw + (i - 5) * cw + (i - 5) * info.sp
+        let cardT = strT + info.sp
+        ctx.strokeRect(cardL, cardT, cw, cw)
+        ctx.fillStyle = this.color.cardText
+        ctx.font = '28px YaHei'
+        ctx.fillText(cards[i].name, cardL+14, cardT+36)
+        for (let j in cards[i].cat) {
+          ctx.fillText(TypeInfo[cards[i].cat[j]], cardL+14, cardT+120+j*40)
+        }
+      }
+      // first button
+      ctx.textBaseline="top"
+      ctx.fillStyle = this.color.gold
+      ctx.fillText(this.game.gold, strL+sp, strT+sp)
+      // second redeal button
+      ctx.textAlign = 'end'
+      ctx.fillStyle = this.color.reDealButton
+      ctx.fillRect(strL+sp, strT+sp*2+bth, cw, bth)
+      ctx.fillStyle = 'white'
+      ctx.fillText(this.game.costRedeal, strL+sp+cw-50, strT+sp*2+bth+40)
+      //third upgrade button
+      ctx.fillStyle = this.color.upgradeButton
+      ctx.fillRect(strL+sp, strT+sp*3+bth*2, cw, bth)
+      ctx.fillStyle = 'white'
+      ctx.fillText('level'+this.game.lvl, strL+sp+cw-50, strT+sp*3+bth*2+10)
+      ctx.fillText(this.game.costUpgrade, strL+sp+cw-50, strT+sp*3+bth*2+40)
+    },
   }
 }
 </script>
