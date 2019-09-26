b82229c0fbe83d36a626061c0ffee84abafafab1
EasonApolo <787817128@qq.com>
2019-09-26 17:10:04 +0800

    0.0.21.10
    update dist

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,6 @@ module.exports = {
         public: '0.0.0.0:8080',
         hot: true,
         disableHostCheck: true
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/'
 }
